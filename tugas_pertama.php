759dfd7df39c59bc3859c8058da4dc41c8bac598
bimowibowo <63335246+bimowibowo@users.noreply.github.com>
2020-04-08 19:30:11 +0700

    menambahkan email dan no hp

--- tugas_pertama.php
+++ tugas_pertama.php
@@ -2,5 +2,7 @@
     echo "Nama : Bimo Prasetyo Wibowo <br>";
     echo "Kelas : XI RPL 2 <br>";
     echo "Sekolah : SMK Taruna Bhakti <br>";
-    echo "Alamat : Jl.Jabel no 86 Rt 01/ Rw 008 Sukamaju Baru Tapos-Depok"
+    echo "Alamat : Jl.Jabel no 86 Rt 01/ Rw 008 Sukamaju Baru Tapos-Depok <br>"
+    echo "No hp : 089677831739 <br>"
+    echo "Email : bimoprasetyowibowo@gmail.com <br>"
 ?>
