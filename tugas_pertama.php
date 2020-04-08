12c506823b474aece4a3f610b49496190ec00c64
bimowibowo <63335246+bimowibowo@users.noreply.github.com>
2020-04-08 19:23:06 +0700

    tugas pertama pbwp

---
+++ tugas_pertama.php
@@ -0,0 +1,6 @@
+<?php
+    echo "Nama : Bimo Prasetyo Wibowo <br>";
+    echo "Kelas : XI RPL 2 <br>";
+    echo "Sekolah : SMK Taruna Bhakti <br>";
+    echo "Alamat : Jl.Jabel no 86 Rt 01/ Rw 008 Sukamaju Baru Tapos-Depok"
+?>
